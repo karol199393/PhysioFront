5f04570904da0f98afd528489c5f54575a41f11c
Karol <114170012+karol199393@users.noreply.github.com>
2024-05-19 09:49:29 +0200

    add reccomendations

--- src/components/Zalecenia.js
+++ src/components/Zalecenia.js
@@ -2,42 +2,30 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import './zalecenia.css';
 
-const TrainingComponent = () => {
-    const [trainings, setTrainings] = useState([]);
-    const [exercises, setExercises] = useState([]);
-    const [selectedExercise, setSelectedExercise] = useState(null);
+const Zalecenia = () => {
+    const [recommendations, setRecommentadions] = useState([]);
+    
+
+
 
     useEffect(() => {
-        axios.get('http://localhost:8080/api/v1/recomendations')
-            .then(response => {
-                setTrainings(response.data);
-            })
-            .catch(error => {
-                console.log(error);
-            });
-    }, []);
-
-    useEffect(() => {
-        axios.get('http://localhost:8080/api/v1/exercises')
-            .then(response => {
-                setExercises(response.data);
-            })
-            .catch(error => {
-                console.log(error);
-            });
-    }, []);
-
-    const assignExerciseToTraining = (exerciseId, trainingId) => {
-        fetch(`http://localhost:8080/api/v1/exercises/${exerciseId}/assign/${trainingId}`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-        })
+        fetch('http://localhost:8080/api/v1/getAllRecommendations')
         .then(response => response.json())
-        .then(data => console.log('Success:', data))
+        .then(data => setRecommentadions(data))
         .catch(error => console.error('Error:', error));
-    };
+}, []);
+
+    useEffect(() => {
+        axios.get('http://localhost:8080/api/v1/createRecommendations')
+            .then(response => {
+                setRecommentadions(response.data);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }, []);
+
+ 
     return (
         <div>
             <h1>Zalecenia</h1>
@@ -51,10 +39,10 @@ const TrainingComponent = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    {trainings.map(training => (
-                        <tr key={training.id}>
-                            <td>{training.id}</td>
-                            <td>{training.name}</td>
+                    {recommendations.map(recommendations => (
+                        <tr key={recommendations.id}>
+                            <td>{recommendations.id}</td>
+                            <td>{recommendations.recommendation}</td>
                             <td>
                                 <select onChange={(event) => setSelectedExercise(event.target.value)}>
                                     <option value="">Wybierz ćwiczenie</option>
@@ -73,4 +61,4 @@ const TrainingComponent = () => {
 };
 
 
-export default TrainingComponent;
+export default Zalecenia;
